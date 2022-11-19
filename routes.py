7b2d45cf69e4c8530108a3b4ab60511b0f60078f
Yuripacific17 <dongismyusername@gmail.com>
2022-11-19 02:44:56 -0700

    added cors in routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from router import admin_router, affiliate_climate_change_impact_router, affiliate_router, calculator_router, \
     climate_measure_router
 
 app = FastAPI()
 
+origins = [
+
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://impactcalculator.ulivit.ca",
+    "https://impactcalculator.ulivit.ca",
+    "impactcalculator.ulivit.ca"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["Root"])
 async def read_root() -> dict:
