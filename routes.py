7ed625874fe2339eb63033cf0582917bc600c0fc
Yuripacific17 <dongismyusername@gmail.com>
2022-11-19 02:59:59 -0700

    updated cors in vercel.json

--- routes.py
+++ routes.py
@@ -12,13 +12,12 @@ origins = [
     "http://localhost:3000",
     "http://impactcalculator.ulivit.ca",
     "https://impactcalculator.ulivit.ca",
-    "https://impactcalculator.ulivit.ca/",
     "impactcalculator.ulivit.ca"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["origins"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
